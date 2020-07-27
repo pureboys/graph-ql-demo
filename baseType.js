798785e3e92d43356b9650e3c53f18e67ecaab0a
pureboys <yuyuud@gmail.com>
2020-07-27 22:42:40 +0800

    init

---
+++ baseType.js
@@ -0,0 +1,49 @@
+let express = require('express');
+let { graphqlHTTP } = require('express-graphql');
+let { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+let schema = buildSchema(`
+  type proInfo {
+    id: Int
+    proName: String
+    type: Int
+    comment(comment_type:Int):[String]
+  }
+
+  type Query {
+    getProInfo(id: Int): proInfo
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+let root = {
+    getProInfo:({id}) => {
+        const proName = "iphone"
+        const type = 1
+        const comment = ({comment_type}) => {
+            if (comment_type === 1) {
+                return ["我是京东的评论1","我是京东的评论2",];
+            }
+            return  ["我是百科的评论1","我是百科的评论2",];
+        }
+        return {
+            id,
+            proName,
+            type,
+            comment
+        }
+    }
+};
+
+let app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.use(express.static('public'));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
---
+++ helloWorld.js
@@ -0,0 +1,37 @@
+let express = require('express');
+let { graphqlHTTP } = require('express-graphql');
+let { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+let schema = buildSchema(`
+  type Method {
+    counts: Int
+    lists: [String]
+  }
+  type Query {
+    ver: String
+    Method: Method
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+let root = {
+    ver: () => {
+        return '0.1';
+    },
+    Method:() => {
+        return {
+            counts: 2,
+            lists: ["getList", "getInfo"]
+        }
+    }
+};
+
+let app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
